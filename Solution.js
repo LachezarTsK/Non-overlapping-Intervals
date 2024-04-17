16b8987b11ac16a2b60a3c6cc47de16583fbd5c6
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-04-17 23:35:27 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,23 @@
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function (intervals) {
+    intervals.sort((x, y) => x[1] - y[1]);
+
+    let numberOfErasedOverlappingIntervals = 0;
+    let endIntervalValue = intervals[0][1];
+    let index = 1;
+
+    while (index < intervals.length) {
+        if (endIntervalValue > intervals[index][0]) {
+            ++numberOfErasedOverlappingIntervals;
+        } else {
+            endIntervalValue = intervals[index][1];
+        }
+        ++index;
+    }
+
+    return numberOfErasedOverlappingIntervals;
+};
